a0f4f3a30a0c837ef78f26e9a0b743ebc89ca10c
debenz <dennis.benz@uos.de>
2023-02-02 08:49:33 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+install_requires = []
+with open('requirements.txt') as f:
+    for line in f.readlines():
+        req = line.strip()
+        if not req or req.startswith(('-e', '#')):
+            continue
+        install_requires.append(req)
+
+setup(
+    name='jupyterhub-studipauthenticator',
+    version='0.1.0',
+    packages=['studipauthenticator'],
+    url='https://github.com/dennis531/jupyterhub-studip-authenticator',
+    license='',
+    author='debenz',
+    author_email='debenz@uos.de',
+    description='Stud.IP LTI Authentication for Courses',
+    install_requires=install_requires
+)
