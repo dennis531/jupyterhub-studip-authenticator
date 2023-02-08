0a0aaa9f2afd3571592080466b01f6ef19952434
debenz <dennis.benz@uos.de>
2023-02-08 14:53:44 +0100

    Add some cleanups and documentation

--- studipauthenticator/studipauthenticator.py
+++ studipauthenticator/studipauthenticator.py
@@ -1,33 +1,58 @@
-import grp
+import os
+import subprocess
 
 from jupyterhub.handlers import BaseHandler
 from ltiauthenticator import LTIAuthenticator
 from tljh.normalize import generate_system_username
 from tljh.user import ensure_user
 
-import os
-import subprocess
-import shlex
-import hashlib
-
 
 class StudipAuthenticator(LTIAuthenticator):
+    """
+    Stud.IP LTI 1.1 Authenticator for JupyterHub installed by TLJH. This class subclasses the
+    LTIAuthenticator (https://github.com/jupyterhub/ltiauthenticator).
+    Requires an installation of The Littlest Jupyter Hub (https://tljh.jupyter.org/).
+
+    Creates a working directory for each Stud.IP course in which instructors can write and learners can read.
+    """
+
     username_key = "user_id"
 
     _instructor_roles = ["Instructor", "Administrator", "Staff"]
     _course_id = ""
 
     def is_instructor(self, user_roles):
+        """Checks if passed user roles match an instructor role
+
+        Args:
+            user_roles: Assigned lti roles to current user
+
+        Returns:
+            True if instructor role
+        """
         return any([role in user_roles for role in self._instructor_roles])
 
     async def authenticate(  # noqa: C901
             self, handler: BaseHandler, data: dict = None
     ) -> dict:
+        """Authenticates a user and creates a course workspace
+        If no course id or no user id is passed via the lti parameter,
+        the authentication will only be performed by the lti authenticator.
+        Thus, no course workspace will be created.
+
+        Args:
+            handler: JupyterHub's Authenticator handler object.
+            data: optional data object
+
+        Returns:
+            Authentication dictionary
+        """
+        # Perform lti authentication in super method
         result = await super().authenticate(handler, data)
 
         user_roles = handler.get_argument("roles", "Learner").split(",")
         self._course_id = handler.get_argument("context_id", None)
-        course_name = handler.get_argument("context_title", None)
+        # course_name = handler.get_argument("context_title", None)
         user_id = handler.get_argument("user_id", None)
 
         # Do nothing when course id and user id are not provided
@@ -38,7 +63,8 @@ class StudipAuthenticator(LTIAuthenticator):
             #
             # self.log.debug(f"user-name: {result['name']}")
 
-            # Ensure user exists. TODO: Move these functions to custom spawner to prevent this
+            # Ensure user exists.
+            # TODO: Move these functions to custom spawner to prevent this
             system_username = generate_system_username("jupyter-" + result["name"])
             ensure_user(system_username)
 
@@ -91,18 +117,15 @@ class StudipAuthenticator(LTIAuthenticator):
         return result
 
     def pre_spawn_start(self, user, spawner):
-        """
+        """Sets the user working dir to the course working dir before spawner is started
+        Requires the custom spanner of tljh.
 
-        :param user:
-        :param spawner: Custom spanner of tljh
-        :return:
+        Args:
+            user: jupyterhub user
+            spawner: Custom spanner of tljh
         """
         if self._course_id:
             # Set user working dir
             spawner.user_workingdir = f'/srv/data/courses/{self._course_id}'
 
         super().pre_spawn_start(user, spawner)
-
-    # def post_spawn_stop(self, user, spawner):
-    #     pass
-    #     # Todo: Remove sym link of course workspace
